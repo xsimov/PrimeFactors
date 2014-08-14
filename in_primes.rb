53298c6ebba8b5326695e51a13c530843dd37dfb
xsimov <xsimov@gmail.com>
2014-08-14 20:34:30 +0200

    green

--- in_primes.rb
+++ in_primes.rb
@@ -1,5 +1,16 @@
 module Descompose
   def self.in_primes(number)
-    [number]
+    return [1] if number == 1
+    [number] if is_prime?(number)
+  end
+
+  def self.is_prime?(number)
+    return true if number == 2
+    for i in 2..(number-1) do
+      if number % i == 0
+        return false
+      end
+    end
+    true
   end
 end
