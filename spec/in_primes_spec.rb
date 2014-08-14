dfc264493b464d59ff2624937410a94fbce55422
xsimov <xsimov@gmail.com>
2014-08-14 20:14:17 +0200

    red

--- spec/in_primes_spec.rb
+++ spec/in_primes_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../in_primes'
 
-describe "In primes" do
+describe "Descompose in primes" do
   it "should return one if the input is one" do
     expect(Descompose.in_primes(1)).to eq([1])
   end
   
+  it "should return the same number if is prime" do
+    expect(Descompose.in_primes(2)).to eq([2])
+  end
 
 end
